55795c128d020a3eab7384c5d12f0a770a989303
CristiMacovei <me@cristimacovei.dev>
2024-04-17 11:25:49 +0300

    First commit | downloaded skeleton
    Signed-off-by: CristiMacovei <me@cristimacovei.dev>

---
+++ skel/lru_cache.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024, <>
+ */
+
+#include "lru_cache.h"
+#include "utils.h"
+#include <stdio.h>
+#include <string.h>
+
+lru_cache *init_lru_cache(unsigned int cache_capacity)
+{
+	/* TODO */
+	return NULL;
+}
+
+bool lru_cache_is_full(lru_cache *cache)
+{
+	/* TODO */
+	return false;
+}
+
+void free_lru_cache(lru_cache **cache) { /* TODO */ }
+
+bool lru_cache_put(lru_cache *cache, void *key, void *value, void **evicted_key)
+{
+	/* TODO */
+	return false;
+}
+
+void *lru_cache_get(lru_cache *cache, void *key)
+{
+	/* TODO */
+	return NULL;
+}
+
+void lru_cache_remove(lru_cache *cache, void *key) { /* TODO */ }
